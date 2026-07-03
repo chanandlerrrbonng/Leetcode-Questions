737da02efb82013d0918b638144e6b5c0effb4f5
Amrit Kumar Ghoshal <chanandlerrrbonng2005@gmail.com>
2026-07-03 13:50:58 +0530

    Time: 0 ms (100%), Space: 14.6 MB (83.2%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int recurse(vector<vector<int>>& matrix, int row, int col){
-        if(col < 0 || col >= matrix[0].size()) return 1e9;
-        if(row==matrix.size()-1) return matrix[row][col];
-        if(dp[row][col]!=1e9) return dp[row][col];
-        return dp[row][col]=matrix[row][col]+min({recurse(matrix, row+1, col), recurse(matrix, row+1, col-1), recurse(matrix, row+1, col+1)});
-    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        dp.assign(n, vector<int>(m, 1e9));
-        int ans=INT_MAX;
-        for(int i=0; i<m; i++){
-            ans=min(recurse(matrix, 0, i), ans);
+        vector<int>prevrow=matrix[0];
+        vector<int>thisrow(m, INT_MAX);
+        for(int i=1; i<n; i++){
+            for(int j=0; j<m; j++){
+                int left=INT_MAX, right=INT_MAX;
+                if(j>0) left=prevrow[j-1];
+                int middle=prevrow[j];
+                if(j<m-1) right=prevrow[j+1];
+                thisrow[j]=matrix[i][j]+min({left, middle, right});
+            }
+            prevrow=thisrow;
         }
-        return ans;
-        
+        return *min_element(prevrow.begin(), prevrow.end());
     }
 };
